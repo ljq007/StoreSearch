d35ff476ef9a6e60229c43d3a4f90e109f488334
Jack Lee <ljq007@gmail.com>
2013-09-28 13:37:12 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Jack Lee on 13-9-28.
+//  Copyright (c) 2013年 ljq007@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoreSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoreSearch
+//
+//  Created by Jack Lee on 13-9-28.
+//  Copyright (c) 2013年 ljq007@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
