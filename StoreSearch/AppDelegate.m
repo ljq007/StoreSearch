efb478830c9145d59bea1c398e80ec5f0318ad1e
Jack Lee <ljq007@gmail.com>
2013-09-28 13:50:43 +0800

    Add SearchViewController.h,m,xib

--- StoreSearch/AppDelegate.m
+++ StoreSearch/AppDelegate.m
@@ -8,13 +8,17 @@
 
 #import "AppDelegate.h"
 
+#import "SearchViewController.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.viewController = [[SearchViewController alloc] initWithNibName:@"SearchViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StoreSearch
+//
+//  Created by Jack Lee on 13-9-28.
+//  Copyright (c) 2013年 ljq007@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
